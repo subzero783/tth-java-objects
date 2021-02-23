07014d64ce2c8bbc93ae1ad9e1b6a57d705004d9
Gus <gusta1296@gmail.com>
2021-02-22 20:19:18 -0800

    first push

---
+++ Example.java
@@ -0,0 +1,43 @@
+public class Example{
+    public static void main(String[] args){
+        System.out.println("We are making a new PEZ Dispenser");
+        System.out.printf("FUN FACT: There are %d PEZ allowed in every dispenser %n", 
+        PezDispenser.MAX_PEZ);
+        PezDispenser dispenser = new PezDispenser("Yoda");
+        System.out.printf("The dispenser is %s %n",
+        dispenser.getCharacterName());
+
+        if(dispenser.isEmpty()){
+            System.out.println("Dispenser is empty");
+        }
+
+        System.out.println("Filling the dispenser with delicious PEZ...");
+        dispenser.fill();
+        
+        if(!dispenser.isEmpty()){
+            System.out.println("Dispenser is full");
+        }
+
+        while(dispenser.dispense()){
+            System.out.println("Chomp!");
+        }
+
+        if(dispenser.isEmpty()){
+            System.out.println("Ate all the PEZ");
+        }
+
+        dispenser.fill(4);
+        dispenser.fill(2);
+
+        while(dispenser.dispense()){
+            System.out.println("Chomp!");
+        }
+        try {
+            dispenser.fill(500);
+            System.out.println("This will never happen");
+        }catch(IllegalArgumentException iae){
+            System.out.println("Whoa there!");
+            System.out.printf("The error was %s", iae.getMessage());
+        }
+    }
+}
